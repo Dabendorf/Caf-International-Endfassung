ec62d9297cabe852c96eba973f97b9e00f7ed25b
Dabendorf <schramm98@gmail.com>
2015-05-15 11:15:23 +0200

    Spielstände werden nun vor dem Speichern vigenereverschlüsselt, um Spielstandmanipulation zu verhindern.

--- Spielstand.java
+++ Spielstand.java
@@ -13,38 +13,38 @@ import javax.swing.JOptionPane;
 public class Spielstand {
 	
 	private String dateiname = "spielstand.txt";
-	private String schluessel = "Heizoelrueckstossabdaempfung";
+	private char[] schluessel = "Heizoelrueckstossabdaempfung".toCharArray();
 	
 	public void speichern() {
 		Properties spielstand = ladeProperties(dateiname);
-		spielstand.setProperty("spielangefangen",String.valueOf(true));
-		spielstand.setProperty("amZug",String.valueOf(Variablen.getAktSpieler()));
-		spielstand.setProperty("zustand",String.valueOf(Variablen.getZustand()));
-		
+		spielstand.clear();
+		spielstand.setProperty("spielangefangen",verschluesseln(String.valueOf(true)));
+		spielstand.setProperty("amZug",verschluesseln(String.valueOf(Variablen.getAktSpieler())));
+		spielstand.setProperty("zustand",verschluesseln(String.valueOf(Variablen.getZustand())));
 		for(int i=0;i<2;i++) {
-			spielstand.setProperty("spielername"+i,Variablen.getSpieler(i).getName());
-			spielstand.setProperty("spielerpunkte"+i,String.valueOf(Variablen.getSpieler(i).getPunkte()));
+			spielstand.setProperty("spielername"+i,verschluesseln(Variablen.getSpieler(i).getName()));
+			spielstand.setProperty("spielerpunkte"+i,verschluesseln(String.valueOf(Variablen.getSpieler(i).getPunkte())));
 			for(int h=0;h<5;h++) {
-				spielstand.setProperty("handkarte"+i+"-"+h,String.valueOf(Variablen.getSpieler(i).getHandkarten().get(h)));
+				spielstand.setProperty("handkarte"+i+"-"+h,verschluesseln(String.valueOf(Variablen.getSpieler(i).getHandkarten().get(h))));
 			}
 		}
-		spielstand.setProperty("anzahlGastkarten",String.valueOf(Variablen.getGastkarten().size()));
-		spielstand.setProperty("anzahlLaenderkarten",String.valueOf(Variablen.getLaenderkarten().size()));
-		spielstand.setProperty("anzahlBarkarten",String.valueOf(Variablen.getBarkarten().size()));
+		spielstand.setProperty("anzahlGastkarten",verschluesseln(String.valueOf(Variablen.getGastkarten().size())));
+		spielstand.setProperty("anzahlLaenderkarten",verschluesseln(String.valueOf(Variablen.getLaenderkarten().size())));
+		spielstand.setProperty("anzahlBarkarten",verschluesseln(String.valueOf(Variablen.getBarkarten().size())));
 		for(int i=0;i<Variablen.getGastkarten().size();i++) {
-			spielstand.setProperty("gastkarte"+i,String.valueOf(Variablen.getGastkarten().get(i)));
+			spielstand.setProperty("gastkarte"+i,verschluesseln(String.valueOf(Variablen.getGastkarten().get(i))));
 		}
 		for(int i=0;i<Variablen.getLaenderkarten().size();i++) {
-			spielstand.setProperty("laenderkarte"+i,String.valueOf(Variablen.getLaenderkarten().get(i)));
+			spielstand.setProperty("laenderkarte"+i,verschluesseln(String.valueOf(Variablen.getLaenderkarten().get(i))));
 		}
 		for(int i=0;i<Variablen.getBarkarten().size();i++) {
-			spielstand.setProperty("barkarte"+i,String.valueOf(Variablen.getBarkarten().get(i)));
+			spielstand.setProperty("barkarte"+i,verschluesseln(String.valueOf(Variablen.getBarkarten().get(i))));
 		}
 		for(int i=0;i<Variablen.getStuehle().size();i++) {
-			spielstand.setProperty("stuhl"+i,String.valueOf(Variablen.getStuehle().get(i).getGast()));
+			spielstand.setProperty("stuhl"+i,verschluesseln(String.valueOf(Variablen.getStuehle().get(i).getGast())));
 		}
 		for(int i=0;i<Variablen.getTische().size();i++) {
-			spielstand.setProperty("tisch"+i,String.valueOf(Variablen.getTische().get(i).getLaenderkarte()));
+			spielstand.setProperty("tisch"+i,verschluesseln(String.valueOf(Variablen.getTische().get(i).getLaenderkarte())));
 		}
 		
 		try {
@@ -56,18 +56,18 @@ public class Spielstand {
 	
 	public void laden() {
 		Properties spielstand = ladeProperties(dateiname);
-		boolean spielgespeichert = Boolean.valueOf(spielstand.getProperty("spielangefangen","false"));
+		boolean spielgespeichert = Boolean.valueOf(entschluesseln(spielstand.getProperty("spielangefangen","false")));
 		if(spielgespeichert) {
 			Meldungen msgbox = new Meldungen();
 			int menue = JOptionPane.showOptionDialog(null,msgbox.altesspielfrage,msgbox.altesspieltitel, JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, msgbox.altesspieloptionen, msgbox.altesspieloptionen[0]);
             if(menue == 0) {
-            	Variablen.setAktSpieler(Integer.valueOf(spielstand.getProperty("amZug")));
-    			Variablen.setZustand(Integer.valueOf(spielstand.getProperty("zustand")));
+            	Variablen.setAktSpieler(Integer.valueOf(entschluesseln(spielstand.getProperty("amZug"))));
+    			Variablen.setZustand(Integer.valueOf(entschluesseln(spielstand.getProperty("zustand"))));
     			for(int i=0;i<2;i++) {
-    				Variablen.getSpieler(i).setName(spielstand.getProperty("spielername"+i));
-    				Variablen.getSpieler(i).setPunkte(Integer.valueOf(spielstand.getProperty("spielerpunkte"+i)));
+    				Variablen.getSpieler(i).setName(entschluesseln(spielstand.getProperty("spielername"+i)));
+    				Variablen.getSpieler(i).setPunkte(Integer.valueOf(entschluesseln(spielstand.getProperty("spielerpunkte"+i))));
     				for(int h=0;h<5;h++) {
-    					Variablen.getSpieler(i).getHandkarten().add(Gastkarte.parseGastkarte(spielstand.getProperty("handkarte"+i+"-"+h)));
+    					Variablen.getSpieler(i).getHandkarten().add(Gastkarte.parseGastkarte(entschluesseln(spielstand.getProperty("handkarte"+i+"-"+h))));
     				}
     			}
     			Programmstart progst = new Programmstart();
@@ -75,24 +75,24 @@ public class Spielstand {
     			Statistikecke.getInfz(0).punktzahlschreiben();
     			Statistikecke.getInfz(1).punktzahlschreiben();
     			Statistikecke.getInfz(Variablen.getAktSpieler()).faerben(true);
-    			int anzahlGastkarten = Integer.valueOf(spielstand.getProperty("anzahlGastkarten"));
-    			int anzahlLaenderkarten = Integer.valueOf(spielstand.getProperty("anzahlLaenderkarten"));
-    			int anzahlBarkarten = Integer.valueOf(spielstand.getProperty("anzahlBarkarten"));
+    			int anzahlGastkarten = Integer.valueOf(entschluesseln(spielstand.getProperty("anzahlGastkarten")));
+    			int anzahlLaenderkarten = Integer.valueOf(entschluesseln(spielstand.getProperty("anzahlLaenderkarten")));
+    			int anzahlBarkarten = Integer.valueOf(entschluesseln(spielstand.getProperty("anzahlBarkarten")));
     			for(int i=0;i<anzahlGastkarten;i++) {
-    				Variablen.getGastkarten().add(Gastkarte.parseGastkarte(spielstand.getProperty("gastkarte"+i)));
+    				Variablen.getGastkarten().add(Gastkarte.parseGastkarte(entschluesseln(spielstand.getProperty("gastkarte"+i))));
     			}
     			for(int i=0;i<anzahlLaenderkarten;i++) {
-    				Variablen.getLaenderkarten().add(Laenderkarte.parseLaenderkarte(spielstand.getProperty("laenderkarte"+i)));
+    				Variablen.getLaenderkarten().add(Laenderkarte.parseLaenderkarte(entschluesseln(spielstand.getProperty("laenderkarte"+i))));
     			}
     			for(int i=0;i<anzahlBarkarten;i++) {
-    				Variablen.getBarkarten().add(Gastkarte.parseGastkarte(spielstand.getProperty("barkarte"+i)));
+    				Variablen.getBarkarten().add(Gastkarte.parseGastkarte(entschluesseln(spielstand.getProperty("barkarte"+i))));
     				Barkartenecke.getBarzelle(i).setGast(Variablen.getBarkarten().get(i));
     			}
     			for(int i=0;i<Variablen.getStuehle().size();i++) {
-    				Variablen.getStuehle().get(i).setStartGast(Gastkarte.parseGastkarte(spielstand.getProperty("stuhl"+i)));
+    				Variablen.getStuehle().get(i).setStartGast(Gastkarte.parseGastkarte(entschluesseln(spielstand.getProperty("stuhl"+i))));
     			}
     			for(int i=0;i<Variablen.getTische().size();i++) {
-    				Variablen.getTische().get(i).setLand(Laenderkarte.parseLaenderkarte(spielstand.getProperty("tisch"+i)));
+    				Variablen.getTische().get(i).setLand(Laenderkarte.parseLaenderkarte(entschluesseln(spielstand.getProperty("tisch"+i))));
     			}
             } else {
             	neuesspiel();
@@ -113,7 +113,7 @@ public class Spielstand {
 	public void loescheSpielstand() {
 		Properties spielstand = ladeProperties(dateiname);
 		spielstand.clear();
-		spielstand.setProperty("spielangefangen",String.valueOf(false));
+		spielstand.setProperty("spielangefangen",verschluesseln(String.valueOf(false)));
 		
 		try {
 			spielstand.store(new FileWriter("dateien/"+dateiname),"Spielstand gespeichert");
@@ -136,6 +136,31 @@ public class Spielstand {
 			
 		}
 		return prop;
-	 }
+	}
+	
+	private String verschluesseln(String original) {
+		char[] temp = original.toCharArray();
+		String crypt = new String("");
+		for(int i=0;i<temp.length;i++) {
+			int result = (temp[i] + schluessel[i%schluessel.length]) % 256;
+			crypt += (char) result;
+        }
+        return crypt;
+    }
+	
+	private String entschluesseln(String verschluesselt) {
+		char[] temp = verschluesselt.toCharArray();
+		String decrypt = new String("");
+		for(int i=0;i<temp.length;i++) {
+			int result;
+            if(temp[i] - schluessel[i%schluessel.length] < 0) {
+            	result =  (temp[i] - schluessel[i%schluessel.length]) + 256;
+            } else {
+            	result = (temp[i] - schluessel[i%schluessel.length]) % 256;
+            }
+            decrypt += (char) result;
+		}
+		return decrypt;
+	}
 
 }
