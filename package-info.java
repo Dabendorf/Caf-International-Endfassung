39e039b8506457d976157e438a931f723d12628e
Dabendorf <schramm98@gmail.com>
2015-06-26 16:22:49 +0200

    Es wurden Umlaute fuer das Java-Dok entfernt.

--- package-info.java
+++ package-info.java
@@ -1,8 +1,8 @@
 /**
  * Bei diesem Projekt handelt es sich um eine Java-Umsetzung des Brettspielklassikers Cafe International.
- * Sie enthaelt alle gaengigen Spielzuege des Spiels für zwei Spieler, eine Punkterechnung und ein Verfahren zum Abspeichern von Spielstaenden.
+ * Sie enthaelt alle gaengigen Spielzuege des Spiels fuer zwei Spieler, eine Punkterechnung und ein Verfahren zum Abspeichern von Spielstaenden.
  * Ausserdem ist eine Datei zur Speicherung von Bestpunktzahlen dabei.
- * Ferner hinzuzufügen ist eine Spielanleitung.
+ * Ferner hinzuzufuegen ist eine Spielanleitung.
  * 
  * @author Lukas Schramm
  * @version 0.3 Alpha - Erste Version mit funktionierendem Spielalgorithmus, inklusive Speichermanagement
