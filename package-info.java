34d10ea4edb72d7174a366edfdab68d2bf1f74bc
Lukas Schramm <schramm98@icloud.com>
2015-08-13 13:46:48 +0200

    Versionszahl aktualisiert und Anleitung hinzugefuegt. Version 1.2.5 ist vorlaeufige Endversion des Spiels.

--- package-info.java
+++ package-info.java
@@ -2,10 +2,10 @@
  * Bei diesem Projekt handelt es sich um eine Java-Umsetzung des Brettspielklassikers Cafe International.
  * Sie enthaelt alle gaengigen Spielzuege des Spiels fuer zwei Spieler, eine Punkterechnung und ein Verfahren zum Abspeichern von Spielstaenden.
  * Ausserdem ist eine Datei zur Speicherung von Bestpunktzahlen dabei.
- * Ferner hinzuzufuegen ist eine Spielanleitung.
  * 
  * @author Lukas Schramm
- * @version 1.2.4 Beta - Vollstaendige Version mit funktionierendem Spielalgorithmus, inklusive Speichermanagement und Bestenliste, Anleitung fehlt
+ * @version 1.2.5 Beta - Vollstaendige Version mit funktionierendem Spielalgorithmus, inklusive Speichermanagement, Bestenliste und Anleitung.
+ * Vorlaeufige Endfassung.
  * 
  *
  */
